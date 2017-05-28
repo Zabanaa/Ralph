4ca3cc24eb7b7d849e9f0e7a246fafaaf8e7e352
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-05-28 15:36:31 +0200

    append script to crontab

--- email_reminder/run_example.sh
+++ email_reminder/run_example.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+""" IMPORTANT: RENAME this file to run.sh """
+
 # Set Environment Variables
 export KC_GMAIL_USERNAME="MY GMAIL USERNAME"
 export BG93_GMAIL_USERNAME="MY SECONDARY GMAIL USERNAME"
 export BG93_GMAIL_PASSWORD="MY SECONDARY GMAIL PASSWORD"
 
 LOG_FILE="/var/log/email_reminder.log"
+SCRIPT_PATH="/home/zabana/projects/Ralph/email_reminder/run.sh"
 
 # If /var/log/email_reminder.log does not exist
 # create the file and chmod 666 it
@@ -16,6 +19,12 @@ if [[ ! -f "$LOG_FILE" ]]; then
     sudo chmod 666 "$LOG_FILE"
     echo "[log] File created."
 fi
+
+# Add script to the user's crontab
+# It will be scheduled to run every month on the 28th at 9:30 AM
+echo "[log] Appending script to crontab"
+(crontab -l ; echo "30 9 28 * * $SCRIPT_PATH")| crontab -
+echo "[log] Done"
 
 # Run the script
 echo "[log] Running script ..."
