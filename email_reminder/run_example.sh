851cee38a2f58894d35d7e81a954ed7b0bf012d8
Karim Cheurfi <karim.cheurfi@gmail.com>
2017-05-28 15:20:51 +0200

    create log file and chmod it if it doesn't already exist

--- email_reminder/run_example.sh
+++ email_reminder/run_example.sh
@@ -2,12 +2,21 @@
 
 # Set Environment Variables
 export KC_GMAIL_USERNAME="MY GMAIL USERNAME"
-export BG93_GMAIL_USERNAME="MY SECONDARY ACCOUNT GMAIL USERNAME"
-export BG93_GMAIL_PASSWORD="MY SECONDARY ACCOUNT GMAIL PASSWORD"
+export BG93_GMAIL_USERNAME="MY SECONDARY GMAIL USERNAME"
+export BG93_GMAIL_PASSWORD="MY SECONDARY GMAIL PASSWORD"
+
+LOG_FILE="/var/log/email_reminder.log"
+
+# If /var/log/email_reminder.log does not exist
+# create the file and chmod 666 it
+
+if [[ ! -f "$LOG_FILE" ]]; then
+    echo "[log] $LOG_FILE does not exist. Creating it ..."
+    sudo touch "$LOG_FILE"
+    sudo chmod 666 "$LOG_FILE"
+    echo "[log] File created."
+fi
 
 # Run the script
+echo "[log] Running script ..."
 python email_reminder.py
-
-
-
-
